367f4a1c1caa0421e37da518a27a09be7582fd38
Mike Smedes <mike@clearing.com>
2021-12-13 19:26:41 -0500

    initial commit

---
+++ git.sh
@@ -0,0 +1,10 @@
+# General git helpers
+
+alias ga="git add ."
+alias gb="git branch"
+alias gcam="git commit -am"
+alias gd="git diff"
+alias gl="git log"
+alias gp="git push -u"
+alias gpu="git push --set-upstream origin \$(git branch --show-current)"
+alias gnb="git checkout -b"
